bf256ddc9e259cd03f41d4400db16a42ba02c5bb
oliver halvey <oliverhalvey310@yahoo.com>
2023-02-16 07:46:47 -0800

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Oliver Halvey on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
